411859c71a6984392f0c0611e375bffa148e979c
maksym.onyshchenko <devops@maxim.run>
2022-10-10 17:09:28 -0700

    feat: mvp

--- main.go
+++ main.go
@@ -1,48 +1,30 @@
 package main
 
 import (
+	"encoding/base64"
 	"encoding/json"
+	"flag"
 	"fmt"
 	"io/ioutil"
-	"log"
 	"net/http"
-
-	// "reflect"
+	"os"
 	"strconv"
 	"time"
 
+	// "errors"
 	"github.com/gin-gonic/gin"
 )
 
-// albums slice to seed record album data.
-
-var test = map[string]Priorities{"1": {Low: Severity{Count: 1, Incidents: []Incident{Incident{Priority: "low", Employee_id: 1, Timestamp: 1234567890}}}, Medium: Severity{Count: 1, Incidents: []Incident{Incident{Priority: "medium", Employee_id: 1, Timestamp: 1234567890}}}, High: Severity{Count: 1, Incidents: []Incident{Incident{Priority: "high", Employee_id: 1, Timestamp: 1234567890}}}, Critical: Severity{Count: 1, Incidents: []Incident{Incident{Priority: "critical", Employee_id: 1, Timestamp: 1234567890}}}}}
-
-// var test = []employee{
-// 	{Employee: ["1": [Count: 1, Incidents: [Priority: "low", Employee_id: 1, Timestamp: 1234567890]]]},
-// 	{Employee: ["2": [Count: 1, Incidents: [Priority: "low", Employee_id: 2, Timestamp: 1234567890]]]},
-// 	{Employee: ["3": [Count: 1, Incidents: [Priority: "low", Employee_id: 3, Timestamp: 1234567890]]]}
-// }
-
-func main() {
-	// router := gin.Default()
-	// router.GET("/incidents", getIncidents)
-	// router.GET("/albums/:id", getAlbumByID)
-	// router.POST("/albums", postAlbums)
-
-	// router.Run("localhost:8080")
-	// fmt.Println(parseIncidents())
-	mergeIncidents()
-
+//  todo / add unauthorized , misuse, disclosure, modification, destruction
+type Configuration struct {
+	Port             int    `json:"port"`
+	ConnectionString string `json:"connection_string"`
+	AuthUsername     string `json:"auth_username"`
+	AuthPassword     string `json:"auth_password"`
+	Timeout          int    `json:"timeout"`
 }
 
-// getAlbums responds with the list of all albums as JSON.
-func getIncidents(c *gin.Context) {
-
-	mergeIncidents()
-	c.IndentedJSON(http.StatusOK, mergeIncidents())
-	// c.IndentedJSON(http.StatusOK, test)
-}
+var configuration Configuration
 
 type Response struct {
 	Results []Incident
@@ -62,100 +44,117 @@ type Priorities struct {
 	Critical Severity `json:"critical"`
 }
 
-// func (p Priorities) fillDefaults()  {
-
-// //   return &Priorities{Low: Severity{}, Medium: Severity{}, High: Severity{}, Critical: Severity{}}
-// }
-
 type Severity struct {
 	Count     int32      `json:"count"`
 	Incidents []Incident `json:"incidents"`
 }
-type Merged map[string]Priorities
 
-func addIncidnetToEmployee(incident Incident, employee_id string) {
-	
+type Merged map[string]*Priorities
+
+func main() {
+	initConfig()
+
+	// setting up gin (web framework)
+	router := gin.Default()
+	router.GET("/incidents", getIncidents)
+	// router.SetTrustedProxies([]string{})
+	router.Run(fmt.Sprintf("localhost:%v", configuration.Port))
 
 }
 
+// getAlbums responds with the list of all albums as JSON.
+func getIncidents(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, mergeIncidents())
+}
+
 func mergeIncidents() map[string]Priorities {
-	// parse incidents from url1
-	// parse incidents from url2
-	// parse incidents from url3
-	// add to test
-	// provide these in config file
-	// url1:="https://incident-api.use1stag.elevatesecurity.io/identities/"
-	url2 := "https://incident-api.use1stag.elevatesecurity.io/incidents/misuse/"
-	// url3 := "https://incident-api.use1stag.elevatesecurity.io/incidents/unauthorized/"
+	url1 := fmt.Sprint(configuration.ConnectionString, "/incidents/misuse/")
+	url2 := fmt.Sprint(configuration.ConnectionString, "/incidents/unauthorized/")
 
 	// declaring a variable for storing incidents of types "misuse" and "unauthorized"
 	merged := Merged{}
-	// merged := make(map[string]Priorities)
-	// var merged []string
 	var response1 Response
-	// var response2 Response
-	json.Unmarshal(querryIncidents(url2), &response1)
-	// json.Unmarshal(querryIncidents(url3), &response2 )
-	
-	// iterating over incidents in the original response
-	for _, incident := range response1.Results {
+	var response2 Response
+	json.Unmarshal(querryIncidents(url1), &response1)
+	json.Unmarshal(querryIncidents(url2), &response2)
+
+	// iterating over incidents in original responses
+	for _, incident := range append(response1.Results, response2.Results...) {
 		employee_id_string := strconv.FormatInt(incident.Employee_id, 10)
 
 		// initialize employee_id if not present
 		if _, ok := merged[employee_id_string]; !ok {
-			merged[employee_id_string] = Priorities{}
+			merged[employee_id_string] = &Priorities{}
 		}
 		switch incident.Priority {
 		case "low":
 			merged[employee_id_string].Low.Count++
+			merged[employee_id_string].Low.Incidents = append(merged[employee_id_string].Low.Incidents, incident)
 		case "medium":
-			// merged[employee_id_string].Medium.Count++
+			merged[employee_id_string].Medium.Count++
+			merged[employee_id_string].Medium.Incidents = append(merged[employee_id_string].Medium.Incidents, incident)
 		case "high":
-			// merged[employee_id_string].High.Count++
+			merged[employee_id_string].High.Count++
+			merged[employee_id_string].High.Incidents = append(merged[employee_id_string].High.Incidents, incident)
 		case "critical":
-			// merged[employee_id_string].Critical.Count++
-			
+			merged[employee_id_string].Critical.Count++
+			merged[employee_id_string].Critical.Incidents = append(merged[employee_id_string].Critical.Incidents, incident)
+
 		}
 
-		fmt.Printf("%+v\n", merged[employee_id_string])
-
-		
-		// merged = append(merged, employee_id_string)
-
 	}
-	// fmt.Printf("Code = %v, Employees = %v", response.Code, response.Employees)
-	// fmt.Println(reflect.TypeOf(response1.Results[0].Employee_id))
-	// fmt.Println(len(response1.Results))
-	// fmt.Printf("%+v\n", response1)
-	// fmt.Printf("%+v\n", merged)
-	// fmt.Println(reflect.TypeOf(response2))
-
-	//
-
-	return map[string]Priorities{}
-
+	// todo / rethink data model
+	normal_merged := map[string]Priorities{}
+	for employee_id, priorities := range merged {
+		normal_merged[employee_id] = *priorities
+	}
+	return normal_merged
 }
 
 func querryIncidents(url string) []byte {
-	client := http.Client{
-		Timeout: time.Second * 10, // Timeout after 2 seconds
-	}
-	req, err := http.NewRequest("GET", url, nil)
-	// todo  / there is a shorter way to catch errors
-	if err != nil {
-		log.Fatal(err)
-	}
-	// todo / configure Auth in more elegant way
-	req.Header.Set("Authorization", "Basic ")
+	client := http.Client{Timeout: time.Second * time.Duration(configuration.Timeout)}
+	req, _ := http.NewRequest("GET", url, nil)
+	req.Header.Set("Authorization", "Basic "+basicAuth(configuration.AuthUsername, configuration.AuthPassword))
 	res, err := client.Do(req)
-	if err != nil {
-		log.Fatal(err)
+	if err != nil || res.StatusCode != 200 {
+		fmt.Printf("Can not read remote data. Response code: %v . Error: %v  ", res.StatusCode, err)
+		os.Exit(1)
 	}
 
 	body, err := ioutil.ReadAll(res.Body)
 	if err != nil {
-		log.Fatal(err)
+		fmt.Printf("Can not read response body. Error: %v  ", err)
+		os.Exit(1)
 	}
 	return body
+
+}
+
+func basicAuth(username, password string) string {
+	auth := username + ":" + password
+	return base64.StdEncoding.EncodeToString([]byte(auth))
+}
+
+func initConfig() {
+	// getting configuration values from the config file
+	configPath := flag.String("config", "./config/config-development.json", "a path to configuration file")
+	file, err := os.Open(*configPath)
+	if err != nil {
+		fmt.Printf("Can not open config file\n")
+		return
+	}
+	decoder := json.NewDecoder(file)
+	configuration = Configuration{}
+	err = decoder.Decode(&configuration)
+	if err != nil {
+		fmt.Printf("Can not decode config file\n")
+		return
+	}
+	secretsPath := flag.String("secrets-path", "./config/secrets", "a path where auth_username and auth_password secrets are stored as files.")
+	AuthUsernameFile, _ := os.ReadFile(fmt.Sprint(*secretsPath, "/auth_username"))
+	AuthPasswordFile, _ := os.ReadFile(fmt.Sprint(*secretsPath, "/auth_password"))
+
+	configuration.AuthUsername = string(AuthUsernameFile)
+	configuration.AuthPassword = string(AuthPasswordFile)
 
 }
